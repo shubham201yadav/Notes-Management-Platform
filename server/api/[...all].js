0ba1767df34b14054de882752a54c7c5ccb44640
Sampad Biswas <bsampad316@gmail.com>
2026-03-06 02:57:35 +0530

    fix: use serverless api handler and init mongodb on invoke

--- server/api/[...all].js
+++ server/api/[...all].js
@@ -1,3 +1,28 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = require("../app");
 
-module.exports = app;
+let isConnected = false;
+
+async function connectToDatabase() {
+	if (isConnected) {
+		return;
+	}
+
+	if (!process.env.MONGO_URI) {
+		throw new Error("MONGO_URI is not defined");
+	}
+
+	await mongoose.connect(process.env.MONGO_URI);
+	isConnected = true;
+}
+
+module.exports = async (req, res) => {
+	try {
+		await connectToDatabase();
+		return app(req, res);
+	} catch (error) {
+		console.error("Serverless startup error:", error);
+		return res.status(500).json({ message: "Server failed to start" });
+	}
+};
